03d7b299f68f96a85bd6584a9017c567aaa6b851
linich <jorge_soto_1999@hotmail.com>
2024-04-19 02:57:49 -0400

    ignore en carpeta assets

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import { pdfjs } from "react-pdf";
 import PdfViewer from "./PdfViewer";
-import pdfurl from "./assets/hola2.pdf";
+import pdfurl from "./assets/librobueno.pdf";
 import Logo from "./assets/logo-uct.png"
 function App() {
   pdfjs.GlobalWorkerOptions.workerSrc = `//cdnjs.cloudflare.com/ajax/libs/pdf.js/${pdfjs.version}/pdf.worker.js`;
@@ -10,8 +10,14 @@ function App() {
       <div className="pt-5">
         <div className="navbar flex border-2">
           <div className="w-1/3"><img src={Logo} alt="Logo" className="logito"/></div>
-          <div className="w-2/3 flex justify-start"><h1 className="font-bold text-white">Libro de don Ciro</h1></div>
+          <div className="w-2/3 flex justify-start"><h1 className="font-bold text-white">Curioso</h1></div>
+          
         </div>  
+        <div className="navbar  flex border-2">
+        <div className="w-2/3 flex justify-start"><h2 className="font-bold text-white">¡El maravilloso mundo de las matematicas!</h2></div>
+          
+        </div>  
+        
 
         <div>
           <PdfViewer pdfUrl={pdfurl} />
