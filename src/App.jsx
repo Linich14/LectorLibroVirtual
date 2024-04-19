180e871dc27889bc2791cce342c2da2496b9f333
DPBascur <danielpa423@gmail.com>
2024-04-19 05:02:02 -0400

    Mejoras visuales

--- src/App.jsx
+++ src/App.jsx
@@ -1,30 +1,48 @@
 import { pdfjs } from "react-pdf";
 import PdfViewer from "./PdfViewer";
 import pdfurl from "./assets/librobueno.pdf";
-import Logo from "./assets/logo-uct.png"
+import Ciro1 from "./assets/av1.png"
+import Ciro2 from "./assets/av2.png"
+import foot from "./assets/Footer.png"
 function App() {
   pdfjs.GlobalWorkerOptions.workerSrc = `//cdnjs.cloudflare.com/ajax/libs/pdf.js/${pdfjs.version}/pdf.worker.js`;
 
   return (
     <>
-      <div className="pt-5">
-        <div className="navbar flex border-2">
-          <div className="w-1/3"><img src={Logo} alt="Logo" className="logito"/></div>
-          <div className="w-2/3 flex justify-start"><h1 className="font-bold text-white">Curioso</h1></div>
-          
+      <div className="">
+        <div className="p-2">
+          <div className="flex border-4 bg-[#6803F7] border-[#06004A] rounded-lg p-3 shadow-md overflow-hidden">
+            <div className="flex w-13">
+              <img src={Ciro2} width="100vh"></img>
+            </div>
+            <div className="grow h-1">
+              <h1 className="font-bold text-white">Curioso</h1>
+              <hr></hr>
+              <h3 className="font-bold text-white">¡El maravilloso mundo de las matemáticas! Para descubrir, motivarse y aprender.</h3>
+            </div>
+            <div className="flex w-13">
+              <img src={Ciro1} width="100vh"></img>
+            </div>
+          </div>
         </div>  
-        <div className="navbar  flex border-2">
-        <div className="w-2/3 flex justify-start"><h2 className="font-bold text-white">¡El maravilloso mundo de las matematicas!</h2></div>
-          
-        </div>  
-        
-
         <div>
           <PdfViewer pdfUrl={pdfurl} />
         </div>
       </div>
-
-      <div className="footer"></div>
+      <footer className="bg-[#0057FF] border-4 border-[#06004A] rounded-lg p-3 shadow-md overflow-hidden">
+        <div className="bg-[#06004A] grid grid-cols-3">
+          <div className="p-2"><p>Ciro González Mallo</p></div>
+          <div className="p-2"><p>Claudia González Algería</p></div>
+          <div className="p-2"><p>Genoveva Garrido Pedreros</p></div>
+        </div>
+        <div className="flex justify-center"><img src={foot}></img></div>
+        <div className="bg-[#06004A] grid grid-cols-3">
+          <div className="p-2"><p>Pagina diseñada por:</p></div>
+          <div className="p-2"><p>Jorge Eduardo Soto</p></div>
+          <div className="p-2"><p>Daniel Felipe Peña</p></div>
+          <p></p>
+        </div>
+      </footer>
     </>
   );
 }
--- src/PdfViewer.jsx
+++ src/PdfViewer.jsx
@@ -22,40 +22,40 @@ const PdfViewer = ({ pdfUrl }) => {
   };
 
   return (
-    <div className="container container-3d flex flex-col md:flex-row justify-center items-center">
-      <div className="pr-4 mb-4 md:mb-0">
+    <div className="pb-5">
+      <div className="">
         <button onClick={handlePrevPage} className="border-1 border-white">
           Anterior 🡰
         </button>
-      </div>
-      <div className="border-2 border-black bg-yellow-950 p-3">
-        <Document
-          file={pdfUrl}
-          onLoadSuccess={onDocumentLoadSuccess}
-          className={"md"}
-        >
-          <FlipPage
-            orientation="horizontal"
-            width={1400}
-            height={900}
-            uncutPages={true}
-            ref={flipPageRef}
-          >
-            {Array.from(new Array(Math.ceil(numPages / 2)), (el, index) => (
-              <div key={`page_${index + 1}`} style={{ display: "flex" }}>
-                <Page pageNumber={index * 2 + 1} height={900} />
-                {index * 2 + 2 <= numPages && (
-                  <Page pageNumber={index * 2 + 2} height={900} />
-                )}
-              </div>
-            ))}
-          </FlipPage>
-        </Document>
-      </div>
-      <div className="pl-4">
         <button onClick={handleNextPage} className="border-1 border-white">
-          Siguiente 🡲
+         🡲 Siguiente 
         </button>
+      </div>
+      <div className="container-3d">
+        <div className="border-2 border-black bg-yellow-950 p-3">
+          <Document
+            file={pdfUrl}
+            onLoadSuccess={onDocumentLoadSuccess}
+            className={"md"}
+          >
+            <FlipPage
+              orientation="horizontal"
+              width={1350}
+              height={900}
+              uncutPages={true}
+              ref={flipPageRef}
+            >
+              {Array.from(new Array(Math.ceil(numPages / 2)), (el, index) => (
+                <div key={`page_${index + 1}`} style={{ display: "flex" }}>
+                  <Page pageNumber={index * 2 + 1} height={900} />
+                  {index * 2 + 2 <= numPages && (
+                    <Page pageNumber={index * 2 + 2} height={900} />
+                  )}
+                </div>
+              ))}
+            </FlipPage>
+          </Document>
+        </div>
       </div>
     </div>
   );
