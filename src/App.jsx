47afaaa403f01238d8dbad8013a93e11282f2e01
linich <jorge_soto_1999@hotmail.com>
2024-04-19 06:14:04 -0400

    Refactorizacion de variables, ajuste CSS y preparacion para primer release oficial

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import { pdfjs } from "react-pdf";
 import PdfViewer from "./PdfViewer";
-import pdfurl from "./assets/librobueno.pdf";
-import Ciro1 from "./assets/av1.png"
-import Ciro2 from "./assets/av2.png"
+import pdfurl from "./assets/librocurioso.pdf";
+import CiroDerecha from "./assets/av1.png"
+import CiroIzquierda from "./assets/av2.png"
 import foot from "./assets/Footer.png"
+
 function App() {
+  // Configuracion de pdfjs para cargar el worker desde un CDN en lugar de cargarlo localmente
   pdfjs.GlobalWorkerOptions.workerSrc = `//cdnjs.cloudflare.com/ajax/libs/pdf.js/${pdfjs.version}/pdf.worker.js`;
 
   return (
@@ -13,15 +15,15 @@ function App() {
         <div className="p-2">
           <div className="flex border-4 bg-[#6803F7] border-[#06004A] rounded-lg p-3 shadow-md overflow-hidden">
             <div className="flex w-13">
-              <img src={Ciro2} width="100vh"></img>
+              <img src={CiroIzquierda} width="100vh"></img>
             </div>
             <div className="grow h-1">
               <h1 className="font-bold text-white">Curioso</h1>
-              <hr></hr>
-              <h3 className="font-bold text-white">¡El maravilloso mundo de las matemáticas! Para descubrir, motivarse y aprender.</h3>
+              <hr ></hr>
+              <h2 className="font-bold text-white">¡El maravilloso mundo de la matemática! Para descubrir, motivarse y aprender.</h2>
             </div>
             <div className="flex w-13">
-              <img src={Ciro1} width="100vh"></img>
+              <img src={CiroDerecha} width="100vh"></img>
             </div>
           </div>
         </div>  
@@ -29,17 +31,17 @@ function App() {
           <PdfViewer pdfUrl={pdfurl} />
         </div>
       </div>
-      <footer className="bg-[#0057FF] border-4 border-[#06004A] rounded-lg p-3 shadow-md overflow-hidden">
-        <div className="bg-[#06004A] grid grid-cols-3">
+      <footer className="bg-[#0057FF] border-4 border-[#06004A] rounded-lg p-3 shadow-md overflow-hidden ">
+        <div className="bg-[#06004A] grid grid-cols-3 rounded-md">
           <div className="p-2"><p>Ciro González Mallo</p></div>
           <div className="p-2"><p>Claudia González Algería</p></div>
           <div className="p-2"><p>Genoveva Garrido Pedreros</p></div>
         </div>
         <div className="flex justify-center"><img src={foot}></img></div>
-        <div className="bg-[#06004A] grid grid-cols-3">
+        <div className="bg-[#06004A] grid grid-cols-3 rounded-md ">
           <div className="p-2"><p>Pagina diseñada por:</p></div>
-          <div className="p-2"><p>Jorge Eduardo Soto</p></div>
-          <div className="p-2"><p>Daniel Felipe Peña</p></div>
+          <div className="p-2"><p><a href="https://github.com/Linich14">Jorge Soto C.</a></p></div>
+          <div className="p-2"><p><a href="https://github.com/DPBascur">Daniel Peña B.</a></p></div>
           <p></p>
         </div>
       </footer>
--- src/PdfViewer.jsx
+++ src/PdfViewer.jsx
@@ -3,59 +3,72 @@ import { Document, Page } from "react-pdf";
 import FlipPage from "react-flip-page";
 import "react-pdf/dist/Page/TextLayer.css";
 import "react-pdf/dist/Page/AnnotationLayer.css";
+import "react-pdf/dist/esm/Page/TextLayer.css"
 import "./App.css";
 
 const PdfViewer = ({ pdfUrl }) => {
+  // Variable para el numero de paginas
   const [numPages, setNumPages] = useState(null);
 
+  // Referencia para el componente FlipPage
   const flipPageRef = useRef();
+
+  // Funciones para cambiar de pagina siguiente
   const handleNextPage = () => {
     flipPageRef.current.gotoNextPage();
   };
 
+  // Funciones para cambiar de pagina anterior
   const handlePrevPage = () => {
     flipPageRef.current.gotoPreviousPage();
   };
 
+  // Funcion para obtener el numero de paginas cuando se carga el documento
   const onDocumentLoadSuccess = ({ numPages }) => {
     setNumPages(numPages);
   };
 
+  // Renderizado del componente PdfViewer con el documento y los botones para cambiar de pagina 
   return (
-    <div className="pb-5">
-      <div className="">
+    <div className="container container-3d flex flex-col md:flex-row justify-center items-center">
+
+      <div className="pr-4 mb-4 md:mb-0">
         <button onClick={handlePrevPage} className="border-1 border-white">
           Anterior 🡰
         </button>
-        <button onClick={handleNextPage} className="border-1 border-white">
-         🡲 Siguiente 
-        </button>
       </div>
-      <div className="container-3d">
-        <div className="border-2 border-black bg-yellow-950 p-3">
-          <Document
-            file={pdfUrl}
-            onLoadSuccess={onDocumentLoadSuccess}
-            className={"md"}
+      <div className="border-2 border-black bg-yellow-950 p-3">
+        <Document
+          file={pdfUrl}
+          onLoadSuccess={onDocumentLoadSuccess}
+          className={"md"}
+        >
+          <FlipPage
+            orientation="horizontal"
+            width={1348}
+            height={900}
+            uncutPages={true}
+            animationDuration={600}
+            showSwipeHint={true}
+            ref={flipPageRef}
           >
-            <FlipPage
-              orientation="horizontal"
-              width={1350}
-              height={900}
-              uncutPages={true}
-              ref={flipPageRef}
-            >
-              {Array.from(new Array(Math.ceil(numPages / 2)), (el, index) => (
-                <div key={`page_${index + 1}`} style={{ display: "flex" }}>
-                  <Page pageNumber={index * 2 + 1} height={900} />
-                  {index * 2 + 2 <= numPages && (
-                    <Page pageNumber={index * 2 + 2} height={900} />
-                  )}
-                </div>
-              ))}
-            </FlipPage>
-          </Document>
-        </div>
+
+            {Array.from(new Array(Math.ceil(numPages / 2)), (el, index) => (
+              <div key={`page_${index + 1}`} style={{ display: "flex" }}>
+                <Page pageNumber={index * 2 + 1} height={900} renderTextLayer={false} />
+                {index * 2 + 2 <= numPages && (
+                  <Page pageNumber={index * 2 + 2} height={900}  renderTextLayer={false}/>
+                )}
+              </div>
+            ))}
+          </FlipPage>
+        </Document>
+      </div>
+
+      <div className="pl-4">
+        <button onClick={handleNextPage} className="border-1 border-white">
+          Siguiente 🡲
+        </button>
       </div>
     </div>
   );
